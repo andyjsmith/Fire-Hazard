2bff2a99e1819f5f40533ff195e2ae16e8a03d1e
andyjsmith <andy@ajsmith.in>
2015-01-18 20:58:01 -0500

    Initial commit

---
+++ desktop/src/firehazard/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package firehazard.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import firehazard.core;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new core(), config);
+	}
+}
---
+++ html/src/firehazard/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package firehazard.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import firehazard.core;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener getApplicationListener () {
+                return new core();
+        }
+}
