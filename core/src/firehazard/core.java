73dae2ed8212d0f84ce3d0d0dbfed207284c0e21
andyjsmith <andy@ajsmith.in>
2015-01-19 12:25:58 -0500

    Changed background to black, added a test particle that runs on opening.

--- core/src/firehazard/core.java
+++ core/src/firehazard/core.java
@@ -3,25 +3,34 @@ package firehazard;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
+//import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.ParticleEffect;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-public class core extends ApplicationAdapter {
+public class core extends ApplicationAdapter{
 	SpriteBatch batch;
-	Texture img;
+	//Texture img;
+
+	private ParticleEffect effect;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		effect = new ParticleEffect();
+		effect.load(Gdx.files.internal("effects/explode.p"), Gdx.files.internal("effects"));
+		effect.setPosition(Gdx.graphics.getWidth() / 2, Gdx.graphics.getHeight() / 2);
+		effect.start();
+		//img = new Texture("badlogic.jpg");
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 34, 0, 1);
+		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(img, 0, 0);
+		//batch.draw(img, 0, 0);
+		effect.draw(batch, Gdx.graphics.getRawDeltaTime());
+		effect.update(Gdx.graphics.getRawDeltaTime());
 		batch.end();
 	}
 }
